381726519aaa5d85b233cda44d3771427ed53402
Juan Herrera <juan.herrera28@hotmail.com>
2022-08-30 23:45:12 -0400

    Primer Commit

---
+++ controllers/auth.js
@@ -0,0 +1,113 @@
+const { response } = require('express');
+// require necesario para mantener el intelligence
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
+const Usuario = require('../models/Usuario');
+
+const crearUsuario = async(req, res = response) => {
+    
+    const { email, password } = req.body;
+
+    try {
+
+        let usuario = await Usuario.findOne({ email: email }); 
+        
+        if( usuario ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario ya existe con ese correo'
+            })
+        }
+
+
+        usuario = new Usuario( req.body );
+
+        // Encriptar contrseña
+        const salt = bcrypt.genSaltSync(); // Cantidad de vueltas o repeticiones para encryptar dato, default 10
+        usuario.password = bcrypt.hashSync( password, salt );
+
+        await usuario.save();
+
+        // Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name );
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(`authController: [crearUsuario]: ${error}`);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el adminsitrador'            
+        })    
+    }
+
+}
+
+const loginUsuario = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const usuario = await Usuario.findOne({ email: email }); 
+        
+        if( !usuario ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario incorrecto'
+            })
+        }
+
+        // confirmar los password
+        const validPassword = bcrypt.compareSync( password, usuario.password );
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Pass incorrecto'
+            })
+        }
+
+        // Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name );
+
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        })
+
+        
+    } catch (error) {
+        console.log(`authController: [loginUsuario]: ${error}`);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el adminsitrador'
+        })  
+    }
+}
+
+const revalidarToken = async(req, res = response) => {
+
+    const { uid, name } = req;
+
+    // generar un nuevo JWT y retornarlo en esta petición
+    const token = await generarJWT( uid, name );
+
+    res.json({
+        ok: true,
+        token
+    })
+}
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken,
+}
---
+++ routes/events.js
@@ -0,0 +1,38 @@
+/* 
+    Rutas de Eventos / Events
+    host + /api/events    
+*/
+
+const { Router } = require('express')
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const { validarCampos } = require('../middlewares/validar-campos')
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
+
+const router = Router();
+
+// Todas tienen que pasar por el JWT
+// todo lo que esté debajo de este use, debe validar jwt. Si tengo alguna ruta publica, solo debo escribirla encima de use
+router.use( validarJWT );
+
+
+router.get('/', getEventos);
+
+router.post('/',
+                [
+                    check('title', 'El titulo es obligatorio').not().isEmpty(),
+                    check('start', 'Fecha de inicio debe ser obligatoria').custom( isDate ),
+                    check('end', 'Fecha de termino es obligatoria').custom( isDate ),
+                    validarCampos
+                ], 
+                crearEvento);
+
+router.put('/:id', actualizarEvento);
+
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
+
+
